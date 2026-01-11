fa39af97121cb5338f44512cbfde426bd403807a
neha <devineha590@gmail.com>
2026-01-11 22:34:04 +0530

    file handling

---
+++ oops/src/com/oops/NotesOfFileHandling.java
@@ -0,0 +1,19 @@
+package com.oops;
+
+public class NotesOfFileHandling {
+
+//	 Q1. What is file handling 
+//	 File Handling in Java refers to the process interacting with files on a computer files system.
+//	 This encompasses operations such as 
+//	 ( Creating, reading from, writng to and deleting.)
+//	--> File handling is used to read data from a file and write data into a file.
+//	
+//	    Q2. Why File Handling is Required?”:
+//		File Handling is required to store data permanently on disk so that it can be read, 
+//		written, updated, or used later, even after the program ends.
+		
+		// FileReader     -> Reads text data from a file
+		// FileWriter     -> Writes text data into a file
+		// BufferedReader -> Reads text from a file line by line efficiently
+
+}
